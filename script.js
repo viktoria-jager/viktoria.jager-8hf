343ba9849755570f64d6485a47fb0284367f4b30
viktoria-jager <viktoria.jager22@gmail.com>
2024-11-12 08:58:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Viktoria Jager");
+
+let country = "Ausztria";
+let city = "Bécs";
+
+console.log(country);
+console.log(city);
+
+let x = 9;
+console.log(x);
+console.log(x + 1);
+
+let y = 20;
+console.log(x + y);
+console.log(x * y);
+
+let z = 2641;
+console.log(z % 7);
+
+let favoriteAnimal = prompt("Mi a kedvenc állatod?");
+console.log("A kedvenc állatod: " + favoriteAnimal);
+
